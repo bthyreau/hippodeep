5309ce602ff1cebb3dd5a814b93bb10917c2df5b
Benjamin Thyreau <benjamin.thyreau@gmail.com>
2017-06-30 12:41:25 +0900

    use the new conv3d api - requires theano >=0.9
    This will enable to run using CPU only
    Flip filters to stay compatible with recent Lasagne/Theano version

--- applyseg_unique.py
+++ applyseg_unique.py
@@ -17,7 +17,9 @@ import theano.tensor as T
 
 import lasagne
 
-from lasagne.layers.dnn import Conv3DDNNLayer, MaxPool3DDNNLayer
+# Note that Conv3DLayer and dnn.Conv3DDNNLayer have opposite filter-fliping defaults
+from lasagne.layers import Conv3DLayer, MaxPool3DLayer
+from lasagne.layers import Upscale3DLayer
 from lasagne.layers import *
 
 
@@ -32,34 +34,6 @@ if not os.path.exists(os.path.join(pickle_cache_path, "output_func.pkl")):
 
     if not os.path.exists(pickle_cache_path):
         os.mkdir(pickle_cache_path)
-
-    class Upscale3DLayer(Layer):
-        def __init__(self, incoming, scale_factor, **kwargs):
-            super(Upscale3DLayer, self).__init__(incoming, **kwargs)
-            self.scale_factor = as_tuple(scale_factor, 3)
-            if self.scale_factor[0] < 1 or self.scale_factor[1] < 1 or self.scale_factor[2] < 1:
-                raise ValueError('Scale factor must be >= 1, not {0}'.format(
-                    self.scale_factor))
-        def get_output_shape_for(self, input_shape):
-            output_shape = list(input_shape)  # copy / convert to mutable list
-            if output_shape[2] is not None:
-                output_shape[2] *= self.scale_factor[0]
-            if output_shape[3] is not None:
-                output_shape[3] *= self.scale_factor[1]
-            if output_shape[4] is not None:
-                output_shape[4] *= self.scale_factor[2]
-            return tuple(output_shape)
-        def get_output_for(self, input, **kwargs):
-            a, b, c = self.scale_factor
-            upscaled = input
-            if c > 1:
-                upscaled = T.extra_ops.repeat(upscaled, c, 4)
-            if b > 1:
-                upscaled = T.extra_ops.repeat(upscaled, b, 3)
-            if a > 1:
-                upscaled = T.extra_ops.repeat(upscaled, a, 2)
-            return upscaled
-
 
     # This broadcast-enabled layer is required to apply a 3d-mask along the feature-dimension
     # From GH PR #633
@@ -158,27 +132,27 @@ if not os.path.exists(os.path.join(pickle_cache_path, "output_func.pkl")):
     # # # #
     # encoding
     # # # #
-    l = Conv3DDNNLayer(l, num_filters = 16, filter_size = (1,1,3), pad = 'valid', name="conv")
-    l = Conv3DDNNLayer(l, num_filters = 16, filter_size = (1,3,1), pad = 'valid', name="conv")
-    l_conv_0 = l = Conv3DDNNLayer(l, num_filters = 16, filter_size = (3,1,1), pad = 'valid', name="conv")
+    l = Conv3DLayer(l, flip_filters=False, num_filters = 16, filter_size = (1,1,3), pad = 'valid', name="conv")
+    l = Conv3DLayer(l, flip_filters=False, num_filters = 16, filter_size = (1,3,1), pad = 'valid', name="conv")
+    l_conv_0 = l = Conv3DLayer(l, flip_filters=False, num_filters = 16, filter_size = (3,1,1), pad = 'valid', name="conv")
 
-    l = l_conv_f1 = Conv3DDNNLayer(l, num_filters = conv_num_filters, filter_size = 3, pad = 'valid', name="conv_f1")
+    l = l_conv_f1 = Conv3DLayer(l, flip_filters=False, num_filters = conv_num_filters, filter_size = 3, pad = 'valid', name="conv_f1")
 
-    l = l_maxpool1 = MaxPool3DDNNLayer(l, pool_size = 2, name ='maxpool1')
+    l = l_maxpool1 = MaxPool3DLayer(l, pool_size = 2, name ='maxpool1')
     l = BatchNormLayer(l, name="batchnorm")
 
-    l = Conv3DDNNLayer(l, num_filters = conv_num_filters, filter_size = (3,3,3), pad = "same", name="conv")
-    l = l_convout1 = Conv3DDNNLayer(l, num_filters = conv_num_filters, filter_size = (3, 3, 3), pad = 'same', name ='convout1', nonlinearity = None)
+    l = Conv3DLayer(l, flip_filters=False, num_filters = conv_num_filters, filter_size = (3,3,3), pad = "same", name="conv")
+    l = l_convout1 = Conv3DLayer(l, flip_filters=False, num_filters = conv_num_filters, filter_size = (3, 3, 3), pad = 'same', name ='convout1', nonlinearity = None)
     l = ElemwiseSumLayer(incomings = [l_maxpool1, l_convout1], name="sum_1s")
     l = NonlinearityLayer(l, nonlinearity = rectify, name="relu")
 
 
     conv_num_filters2 = 48
-    l = l_maxpool2 = MaxPool3DDNNLayer(l, pool_size = 2, name = 'maxpool2')
+    l = l_maxpool2 = MaxPool3DLayer(l, pool_size = 2, name = 'maxpool2')
     l_maxpool2_conv = l
     l = BatchNormLayer(l, name="batchnorm")
-    l = Conv3DDNNLayer(l, num_filters = conv_num_filters2, filter_size = (3,3,3), pad = "same", name="conv")
-    l = l_convout2 = Conv3DDNNLayer(l, num_filters = conv_num_filters2, filter_size = (3, 3, 3), pad = 'same', name ='convout2', nonlinearity = None)
+    l = Conv3DLayer(l, flip_filters=False, num_filters = conv_num_filters2, filter_size = (3,3,3), pad = "same", name="conv")
+    l = l_convout2 = Conv3DLayer(l, flip_filters=False, num_filters = conv_num_filters2, filter_size = (3, 3, 3), pad = 'same', name ='convout2', nonlinearity = None)
     l = ElemwiseSumLayer(incomings = [l_maxpool2_conv, l_convout2], name="sum_2s")
     l = NonlinearityLayer(l, nonlinearity = rectify, name="relu")
 
@@ -187,30 +161,30 @@ if not os.path.exists(os.path.join(pickle_cache_path, "output_func.pkl")):
     # # # #
     l_middle = l
     l = Upscale3DLayer(l, scale_factor = 2, name="upscale")
-    l = Conv3DDNNLayer(l_middle, num_filters = conv_num_filters, filter_size = 3, pad = "same", name="conv")
+    l = Conv3DLayer(l_middle, flip_filters=False, num_filters = conv_num_filters, filter_size = 3, pad = "same", name="conv")
     l = Upscale3DLayer(l, scale_factor = 2, name="upscale")
-    l = l_convout1 = Conv3DDNNLayer(l, num_filters = conv_num_filters, filter_size = 3, pad = 1, name="conv")
+    l = l_convout1 = Conv3DLayer(l, flip_filters=False, num_filters = conv_num_filters, filter_size = 3, pad = 1, name="conv")
     l = Upscale3DLayer(l, scale_factor = 2, name="upscale")
     l_upscale = l
-    l_convout2 = Conv3DDNNLayer(l_upscale, num_filters = 16, filter_size = 3, pad = 1, name="conv")
+    l_convout2 = Conv3DLayer(l_upscale, flip_filters=False, num_filters = 16, filter_size = 3, pad = 1, name="conv")
 
     # Original (before refinement) output
-    l_output1 = Conv3DDNNLayer(l_convout2, num_filters = 1, filter_size = 1, pad = 'same', name="conv_1x", nonlinearity =lasagne.nonlinearities.sigmoid )
+    l_output1 = Conv3DLayer(l_convout2, flip_filters=False, num_filters = 1, filter_size = 1, pad = 'same', name="conv_1x", nonlinearity =lasagne.nonlinearities.sigmoid )
 
     # # #
     # refinement
     # # #
     ## The next output is reusing masked original filters to temptatively improve the network
-    l_blur = Conv3DDNNLayer(l_output1, num_filters=1, filter_size=7, stride=1, pad='same', W=lasagne.init.Constant(1.), b=lasagne.init.Constant(-7*7*7.*.10), nonlinearity=lasagne.nonlinearities.sigmoid)
+    l_blur = Conv3DLayer(l_output1, flip_filters=False, num_filters=1, filter_size=7, stride=1, pad='same', W=lasagne.init.Constant(1.), b=lasagne.init.Constant(-7*7*7.*.10), nonlinearity=lasagne.nonlinearities.sigmoid)
     # in the above, *10 is : threshold at 10% of the smoothed mask (the higher, the smaller the mask)
     for x in l_blur.params.values():
         x.remove("trainable") # never train this, this is for downsampling
 
     l_masked_f1 = ElemwiseMergeLayerBroadcast([l_blur, l_conv_f1], merge_function=T.mul)
-    l_extract = Conv3DDNNLayer(l_masked_f1, num_filters = 47, filter_size = 3, pad = 1, name="extractconv", nonlinearity=leaky_rectify)
+    l_extract = Conv3DLayer(l_masked_f1, flip_filters=False, num_filters = 47, filter_size = 3, pad = 1, name="extractconv", nonlinearity=leaky_rectify)
     l_concat = l = ConcatLayer([l_output1, l_extract], axis=1)
-    l_mix = Conv3DDNNLayer(l_concat, num_filters = 16, filter_size = 3, pad = 1, name="mixconv", nonlinearity=rectify)
-    l_output2 = Conv3DDNNLayer(l_mix, num_filters = 1, filter_size = 1, pad = 'same', name="conv_1x", nonlinearity =lasagne.nonlinearities.sigmoid )
+    l_mix = Conv3DLayer(l_concat, flip_filters=False, num_filters = 16, filter_size = 3, pad = 1, name="mixconv", nonlinearity=rectify)
+    l_output2 = Conv3DLayer(l_mix, flip_filters=False, num_filters = 1, filter_size = 1, pad = 'same', name="conv_1x", nonlinearity =lasagne.nonlinearities.sigmoid )
 
     # Final output
     network = l_output2
